bec04d6e90f8661cffb53d0588de136248aa8579
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 19:33:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Slider from "./components/Slider.jsx";
 export default function App() {
   return (
     <div className="app">
+      {/* Main heading */}
       <h1 className="app-heading">Image Slider</h1>
+      {/* Render Slider component */}
       <Slider />
     </div>
   );
