716b2c953369f45ab57debbfa732660de487d919
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 19:35:56 +0530

    Update Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,8 +3,10 @@ import ImageSlider from "./ImageSlider.jsx";
 import Controls from "./Controls.jsx";
 
 export default function Slider() {
+  // State for current image index
   const [currIndex, setCurrIndex] = useState(0);
-
+  
+  // Array of image paths
   const images = [
     "../bird.jpg",
     "../mountain.jpg",
@@ -17,20 +19,20 @@ export default function Slider() {
     "../sunset.jpg",
   ];
 
+  // Function to go to the next image
   const nextImage = () => {
     setCurrIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
+  // Function to go to the previous image
   const prevImage = () => {
-    setCurrIndex(
-      (prevIndex) => (prevIndex - 1 + images.length) % images.length
-    );
+    setCurrIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
 
   return (
     <div>
-      <ImageSlider image={images[currIndex]} />
-      <Controls nextImage={nextImage} prevImage={prevImage} />
+      <ImageSlider image={images[currIndex]} /> {/* Display current image */}
+      <Controls nextImage={nextImage} prevImage={prevImage} /> {/* Control buttons */}
     </div>
   );
 }
