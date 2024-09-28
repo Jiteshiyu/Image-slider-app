2375963516bf58cab5313fd20aeeb9691fa84866
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 19:34:40 +0530

    Update Controls.jsx

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,9 +1,11 @@
 export default function Controls({ nextImage, prevImage }) {
   return (
     <div className="ctrls">
+      {/* Button to go to the previous image */}
       <button onClick={prevImage} className="prev-btn btn">
         Previous
       </button>
+      {/* Button to go to the next image */}
       <button onClick={nextImage} className="next-btn btn">
         Next
       </button>
