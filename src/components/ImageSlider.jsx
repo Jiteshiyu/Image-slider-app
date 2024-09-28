05c6e3baf80a499f9f1aee0081bb46febeadbcc8
Jiteshiyu <109058809+Jiteshiyu@users.noreply.github.com>
2024-09-28 19:35:03 +0530

    Update ImageSlider.jsx

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,6 +1,7 @@
 export default function ImageSlider({ image }) {
   return (
     <div className="img-slider">
+      {/* Image for the slider */}
       <img src={image} alt="img-slide" className="img" />
     </div>
   );
